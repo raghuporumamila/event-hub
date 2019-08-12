6bbab868a718c1bdd6b08e334127a65779f86687
porumamilla <porumamilla_raghu@yahoo.com>
2019-08-12 17:17:49 -0400

    Changes

--- services/event-hub-schema/src/main/java/com/eventhub/schema/Application.java
+++ services/event-hub-schema/src/main/java/com/eventhub/schema/Application.java
@@ -8,10 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(Application.class);
-        
+		SpringApplication.run(Application.class, args);
+		/*SpringApplication app = new SpringApplication(Application.class);
+
         app.setDefaultProperties(Collections
                 .singletonMap("server.port", "8083"));
-              app.run(args);
+              app.run(args);*/
     }
 }

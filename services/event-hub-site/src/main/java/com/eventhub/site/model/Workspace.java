3bb1298dd37756689e052c1859b2ff35510edfc6
porumamilla <porumamilla_raghu@yahoo.com>
2019-08-22 13:42:20 -0400

    Changes

---
+++ services/event-hub-site/src/main/java/com/eventhub/site/model/Workspace.java
@@ -0,0 +1,20 @@
+package com.eventhub.site.model;
+
+public class Workspace {
+	
+	private String orgId;
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOrgId() {
+		return orgId;
+	}
+	public void setOrgId(String orgId) {
+		this.orgId = orgId;
+	}
+}
